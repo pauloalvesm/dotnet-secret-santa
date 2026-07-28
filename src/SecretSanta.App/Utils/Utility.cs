20d0b45123311e959b54fc9148dca11612492715
pauloalvesm <paulo.alves7351@gmail.com>
2026-07-28 17:44:08 -0300

    fix: update email validation method

--- src/SecretSanta.App/Utils/Utility.cs
+++ src/SecretSanta.App/Utils/Utility.cs
@@ -1,4 +1,5 @@
 ﻿using SecretSanta.App.Models;
+using System.Net.Mail;
 using System.Text.RegularExpressions;
 
 namespace SecretSanta.App.Utils;
@@ -21,17 +22,23 @@ public static class Utility
 
         return true;
     }
-    
-    public static bool ValidateEmailFormat(string providedEmail) 
+
+    public static bool ValidateEmailFormat(string providedEmail)
     {
         if (string.IsNullOrWhiteSpace(providedEmail))
         {
             return false;
         }
 
-        var emailFormat = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
-        
-        return Regex.IsMatch(emailFormat, providedEmail);
+        try
+        {
+            var addr = new MailAddress(providedEmail.Trim());
+            return addr.Address == providedEmail.Trim();
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     public static void SortFriendsByName(List<Friend> friends) 
